113e0fc50808a62bd3616437e7713554768bccc3
Sayajiaji <sayajiaji@gmail.com>
2024-05-30 22:08:34 -0400

    placeholder /start

--- src/commands/user/start.ts
+++ src/commands/user/start.ts
@@ -1,6 +1,7 @@
 import CommandType from "../../models/CommandType";
 import {CacheType, CommandInteraction, SlashCommandBuilder} from "discord.js";
 import Service from "../../services/Service";
+import User from "../../models/user/User";
 
 const command: CommandType = {
     data: new SlashCommandBuilder()
@@ -10,8 +11,14 @@ const command: CommandType = {
         const service = Service.getInstance();
         const user = await service.users.getUser(interaction.user.id);
         if (!user) {
-            // TODO create user
+            const newUser: User = {
+                uid: interaction.user.id,
+                balance: 1000,
+            };
+            await service.users.createUser(newUser);
+            await interaction.reply('PLACEHOLDER: Created a profile for you with $1000.');
         }
+
         // TODO start tutorial
     },
 };
