39c1792c5787d253b9780f976ae6fd3c645d278e
Sayajiaji <sayajiaji@gmail.com>
2024-07-04 16:17:26 -0400

    cap portfolio field

--- src/commands/user/profile.ts
+++ src/commands/user/profile.ts
@@ -80,6 +80,8 @@ const command: CommandType = {
 const generateProfileEmbed = async (userPortfolio: UserPortfolio, yesterdayPrices: Price[], user: User) => {
     let totalPriceDiff = 0;
     let totalYesterdayPrice = 0;
+
+    let i = 1;
     let displayPortfolio = userPortfolio.portfolio.map(hs => {
         const yesterdayPrice = yesterdayPrices.find(p => p.ticker === hs.ticker);
         const priceDiff = (hs.price * hs.quantity - (yesterdayPrice ? yesterdayPrice.close_price * hs.quantity : 0));
@@ -89,7 +91,13 @@ const generateProfileEmbed = async (userPortfolio: UserPortfolio, yesterdayPrice
         totalPriceDiff += priceDiff;
         totalYesterdayPrice += (yesterdayPrice ? yesterdayPrice.close_price * hs.quantity : 0);
 
-        return `${hs.ticker} - ${hs.quantity} share(s) - $${dollarize(hs.price * hs.quantity)}\n${priceDiff >= 0 ? '+' : '-'}$${dollarize(Math.abs(priceDiff))} (${percentDisplay}%)`;
+        if (i < 11) {
+            i++;
+            return `${hs.ticker} - ${hs.quantity} share(s) - $${dollarize(hs.price * hs.quantity)}\n${priceDiff >= 0 ? '+' : '-'}$${dollarize(Math.abs(priceDiff))} (${percentDisplay}%)`;
+        } else {
+            return `... (${userPortfolio.portfolio.length - 10} more holdings)`;
+        }
+
     }).join('\n') || 'No stocks owned.';
 
     const {diff: valueDiff, percent: valueDiffPercent} = await userPortfolio.getDayPortfolioChange();
