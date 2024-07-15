aaab8bea6892fb0a0e0de45cff3668a77c797b1e
Sayajiaji <sayajiaji@gmail.com>
2024-07-15 13:21:04 -0400

    L

--- config.ts
+++ config.ts
@@ -47,7 +47,7 @@ const config: ConfigType = {
             {item: "000", quantity: 1},
             {item: "900", quantity: 1},
         ],
-        "randomWalkBias": 0
+        "randomWalkBias":  1
     },
 };
 
