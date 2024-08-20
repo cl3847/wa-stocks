a1a3a658426207125c067a4b030a489a3689db8c
cl3847 <sayajiaji@gmail.com>
2024-08-20 04:06:13 -0400

    Update config.ts

--- config.ts
+++ config.ts
@@ -54,7 +54,7 @@ const config: ConfigType = {
             {item: "000", quantity: 1},
             {item: "900", quantity: 1},
         ],
-        "randomWalkBias":  0.98,
+        "randomWalkBias":  0.75,
         "minimumLevelRequestAmount": 1,
         "modCommission": 0.75
     },
